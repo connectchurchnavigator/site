a10e696d6bc6ca5595a79c05b5feb2c8d07c2bd7
snaptics2000 <snapticswebdesign@gmail.com>
2026-05-16 01:26:17 +0530

    chore: final visitor connect URL structure /church/[slug]/connect

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1338,7 +1338,7 @@ const VisitorLog = () => {
 
   if (loading) return <div className="p-20 text-center">Loading...</div>;
 
-  const checkInUrl = `${window.location.origin}/${church?.slug}/connect`;
+  const checkInUrl = `${window.location.origin}/church/${church?.slug}/connect`;
 
   return (
     <div className="space-y-6">
