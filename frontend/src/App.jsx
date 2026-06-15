d6d21f381e638401627af7a32e8cffb559c1c2e2
connectchurchnavigator <connect.churchnavigator@gmail.com>
2026-06-15 13:13:20 +0100

    feat: Planner -- Church Picker + Language Filter + Trip Templates -- ChurchNavigator AI Agent [2026-06-15 12:13 UTC]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import ChurchesPage from './pages/ChurchesPage';
+import SearchPage from './pages/SearchPage';
 import ChurchDetailPage from './pages/ChurchDetailPage';
-import EventsPage from './pages/EventsPage';
-import WorshipLeadersPage from './pages/WorshipLeadersPage';
-import MediaTeamsPage from './pages/MediaTeamsPage';
-import FlyerGeneratorPage from './pages/tools/FlyerGeneratorPage';
+import TemplateLibrary from './components/planner/TemplateLibrary';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/churches" element={<ChurchesPage />} />
-            <Route path="/churches/:slug" element={<ChurchDetailPage />} />
-            <Route path="/events" element={<EventsPage />} />
-            <Route path="/worship-leaders" element={<WorshipLeadersPage />} />
-            <Route path="/media-teams" element={<MediaTeamsPage />} />
-            <Route path="/tools/flyer-generator/:eventSlug" element={<FlyerGeneratorPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/church/:id" element={<ChurchDetailPage />} />
+        <Route path="/planner/templates" element={<TemplateLibrary />} />
+      </Routes>
     </Router>
   );
 }
