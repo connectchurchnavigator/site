ebbc0f6c6c31212e5a8d5b50a344324936fed2bc
connectchurchnavigator <connect.churchnavigator@gmail.com>
2026-06-16 00:22:19 +0100

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,15 +12,6 @@ root.render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('SW registered:', registration);
-      })
-      .catch((error) => {
-        console.log('SW registration failed:', error);
-      });
+    navigator.serviceWorker.register('/serviceWorker.js')
   });
 }
-
-reportWebVitals();
